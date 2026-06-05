9b7bbc4acce275b8cb5dafa5c0bdf5ab0a62c9f5
Adeyemo Dorcas Odunola <odunoladorcas1@gmail.com>
2026-06-05 23:46:22 +0100

    Working on local server

---
+++ gcls/src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: 'http://http://dorcas-gcls.name.ng/backend/public/api/',
+};
--- gcls/src/environments/environment.ts
+++ gcls/src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
+  // apiUrl: 'http://dorcas-gcls.name.ng/backend/public/api/',
   apiUrl: 'http://localhost/gcls-prj/backend/public/api/',
 };
